c2bea8e1cd631a436e1663a0ff270b77be068ee0
thach90 <tkach90@yahoo.com.ua>
2020-04-06 02:28:19 +0300

    add additional dishes to the Food Data structure

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -14,4 +14,39 @@ export const foods = [
         name: 'Chicken burrito',
         img: '/img/burrito-chicken.jpg'
     },
+    {
+        id: 4,
+        name: 'Chief burger',
+        img: '/img/cheif-burger.jpg'
+    },
+    {
+        id: 5,
+        name: 'Portuguese pizza',
+        img: '/img/egg-pizza.jpg'
+    },
+    {
+        id: 6,
+        name: 'Hot dog',
+        img: '/img/hoddog.jpg'
+    },
+    {
+        id: 7,
+        name: 'Burger&Fries',
+        img: '/img/burger-potato.jpg'
+    },
+    {
+        id: 8,
+        name: 'Beer',
+        img: '/img/beer.jpg'
+    },
+    {
+        id: 9,
+        name: 'Cheese pizza',
+        img: '/img/slice-pizza.jpg'
+    },
+    {
+        id: 10,
+        name: 'Seafood pizza',
+        img: '/img/shrimp-pizza.jpg'
+    },
 ];
