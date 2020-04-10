e448a321b612d45c45c3490552b8ded7cf482931
thach90 <tkach90@yahoo.com.ua>
2020-04-10 19:36:01 +0300

    add total and tax payments

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -35,6 +35,14 @@ const OrderItem = styled.div`
 `;
 
 export function Order({ orders }) {
+    const subtotal = orders.reduce((total, order) => {
+        return total + getPrice(order);
+    }, 0);
+
+    const tax = subtotal * .07;
+
+    const total = subtotal + tax;
+
     return (
             <OrderStyled >
                 {orders.length === 0 ? (
@@ -53,6 +61,23 @@ export function Order({ orders }) {
                                 </OrderItem>
                             </OrderContainer>
                         ))}
+                        <OrderContainer>
+                            <OrderItem>
+                                <div/>
+                                <div>Sub-Total</div>
+                                <div>{formatPrice(subtotal)}</div>
+                            </OrderItem>
+                            <OrderItem>
+                                <div/>
+                                <div>Tax</div>
+                                <div>{formatPrice(tax)}</div>
+                            </OrderItem>
+                            <OrderItem>
+                                <div/>
+                                <div>Total</div>
+                                <div>{formatPrice(total)}</div>
+                            </OrderItem>
+                        </OrderContainer>
                     </OrderContent>
                 )}
                 <DialogFooter>
